c1865084fc782b48c67c214a1a4025519d2e9ebf
alexjondiaz <alexjondiaz@gmail.com>
2018-04-29 21:00:22 +0200

    fixed media query for search

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: 'AIzaSyCxePK5QxZX9jID8rAAn6nKCw_iDFpNXys',
+  authDomain: 'todo-list-9be98.firebaseapp.com',
+  databaseURL: 'https://todo-list-9be98.firebaseio.com',
+  projectId: 'todo-list-9be98',
+  storageBucket: 'todo-list-9be98.appspot.com',
+  messagingSenderId: '22962782575',
+}
